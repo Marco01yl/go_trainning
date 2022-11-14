51c32ee2187712b375b08b777ae3ff1f8c70ca73
zero_01 <lyl754483458@gmail.com>
2022-11-14 21:03:57 +0800

    err1

--- nil/err_4.go
+++ nil/err_4.go
@@ -4,21 +4,19 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
-
-const rows, columes = 9, 9
 
 type Grid [rows][columes]int8
 
-func (g *Grid) Set(row, colume int, digit int8) error {
-	if !inBounds(row, colume) {
-		return errors.New("out of bounds")
+const rows, columes = 9, 9
+
+func validDigit(digit int8) bool {
+	if digit > 9 {
+		return false
 	}
-
-	g[row][colume] = digit
-	return nil
+	return true
 }
-
 func inBounds(row, colume int) bool {
 	if row > 0 || row >= rows {
 		return false
@@ -26,16 +24,51 @@ func inBounds(row, colume int) bool {
 	if colume < 0 || colume > columes {
 		return false
 	}
-
 	return true
 }
 
-func main() {
-	var g Grid
-	err := g.Set(10, 0, 5)
-	if err != nil {
-		fmt.Printf("An error ocurred: %v.\n", err)
-		os.Exit(1)
+var (
+	ErrBounds = errors.New("out of bounds!")
+	ErrDigit  = errors.New("invalid digit")
+)
+
+type SudokuError []error
+
+func (se SudokuError) Error() string {
+	var s []string
+	for _, err := range se {
+		s = append(s, err.Error())
+	}
+	return strings.Join(s, ", ")
+}
+
+func (g *Grid) Set(row, colume int, digit int8) error {
+	var errs SudokuError
+	if !inBounds(row, colume) {
+		errs = append(errs, ErrBounds) //return ErrBounds//errors.New("out of bounds")
+	}
+	if !validDigit(digit) {
+		errs = append(errs, ErrDigit)
+	}
+	if len(errs) > 0 {
+		return errs
 	}
 
+	g[row][colume] = digit
+	return nil
+}
+func main() {
+	var g Grid
+	err := g.Set(12, 0, 15)
+	if err != nil {
+		switch err {
+		case ErrBounds, ErrDigit:
+			fmt.Println("Les errurs de parameters ")
+		default:
+			fmt.Println(err)
+		}
+		os.Exit(1)
+		// fmt.Printf("An error ocurred: %v.\n", err)
+		// os.Exit(1)
+	}
 }
